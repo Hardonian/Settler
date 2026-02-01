2fc31b087a2f6e0abf7f079ed2bdf1e8b81c1b0b
Scott Hardie <scottrmhardie@gmail.com>
2026-02-01 11:08:37 -0500

    olint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,10 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   rules: {
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
