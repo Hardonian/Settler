01e8b03fe082fa5a40da7e389b4675364cf80e03
Hardonian <118695431+Hardonian@users.noreply.github.com>
2026-05-06 23:12:34 +0000

    chore: Update actions/upload-artifact to v4

---
+++ patch_workflows.sh
@@ -0,0 +1,3 @@
+sed -i 's/actions\/upload-artifact@v3/actions\/upload-artifact@v4/g' .github/workflows/auto-merge.yml
+sed -i 's/actions\/upload-artifact@v3/actions\/upload-artifact@v4/g' .github/workflows/schema-parity-check.yml
+sed -i 's/actions\/upload-artifact@v3/actions\/upload-artifact@v4/g' .github/workflows/classify.yml
