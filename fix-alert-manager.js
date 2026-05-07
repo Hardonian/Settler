ce4f82a036742dbc6862faab92935844f1075d6e
Hardonian <118695431+Hardonian@users.noreply.github.com>
2026-05-07 15:30:12 +0000

    ⚡ Bolt: [performance improvement] optimize N+1 query loop in verify-rls-status script

--- fix-alert-manager.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'packages/adapters/src/alerting/alert-manager.ts';
-let code = fs.readFileSync(file, 'utf8');
-code = code.replace(
-    'const { notificationService } = await import("./notification-service");',
-    'const { notificationService } = await import("./notification-service.js");'
-);
-fs.writeFileSync(file, code);
