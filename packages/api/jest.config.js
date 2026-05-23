e35edf73f46682d1f469c27c2ff0d8daf81f5587
Scott Hardie <scottrmhardie@gmail.com>
2026-05-23 00:13:30 -0400

    ⚡ [performance improvement] Batch queries in Agent Learning Loops (#1075)

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": "ts-jest",
   },
   moduleNameMapper: {
     "^uuid$": require.resolve("uuid"),
